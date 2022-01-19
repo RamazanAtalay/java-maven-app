caf68df791c7ef1979d32f586ff5c82271c81714
Ramazan Atalay <ramazanatalay@gmail.com>
2022-01-19 09:19:45 -0500

    docker key modified

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export IMAGE=$1
-export DOCKER_USER=$2
-export DOCKER_PASSWORD=$3
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
+export DOCKER_CREDS_USR=$2
+export DOCKER_CREDS_PWD=$3
+echo $DOCKER_CREDS_PWD | docker login -u $DOCKER_CREDS_USR --password-stdin
 docker-compose -f docker-compose.yaml up --detach
 echo "success"
